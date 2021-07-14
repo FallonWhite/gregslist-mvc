67c3111de90aa82270c84808ef85447781e288d8
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-14 15:22:53 -0600

    fixed problems

--- app/AppState.js
+++ app/AppState.js
@@ -25,49 +25,49 @@ class AppState extends EventEmitter {
     })
   ]
 
-  @type { House[] }
-houses = [
-  new House({
-    state: 'New York',
-    city: 'Elmira',
-    bedrooms: 10,
-    bathrooms: 4,
-    year: 1876,
-    price: 99900,
-    description: 'Truly amazing Mansard style mansion with gothic influences.',
-    imgUrl: ''
-  }),
-  new House({
-    state: 'North Carolina',
-    city: 'Windsor',
-    bedrooms: 3,
-    bathrooms: 2,
-    year: 1920,
-    price: 110000,
-    description: 'The Butler Nichols House is on the national Register of Historic Places',
-    imgUrl: ''
-  }),
-  new House({
-    state: 'Ohio',
-    city: 'Washington Court House',
-    bedrooms: 4,
-    bathrooms: 3.5,
-    year: 1868,
-    price: 75000,
-    description: 'Own a little piece of history -"The Cherry House"',
-    imgUrl: ''
-  }),
-  new House({
-    state: 'Iowa',
-    city: 'Vinton',
-    bedrooms: 4,
-    bathrooms: 1,
-    year: 1900,
-    price: 120000,
-    description: 'Historic Frank G. Ray House. 3-Story Queen Anne mansion and Carriage House.',
-    imgUrl: ''
-  })
-]
+  /**@type { House[] }*/
+  houses = [
+    new House({
+      state: 'New York',
+      city: 'Elmira',
+      bedrooms: 10,
+      bathrooms: 4,
+      year: 1876,
+      price: 99900,
+      description: 'Truly amazing Mansard style mansion with gothic influences.',
+      imgUrl: ''
+    }),
+    new House({
+      state: 'North Carolina',
+      city: 'Windsor',
+      bedrooms: 3,
+      bathrooms: 2,
+      year: 1920,
+      price: 110000,
+      description: 'The Butler Nichols House is on the national Register of Historic Places',
+      imgUrl: ''
+    }),
+    new House({
+      state: 'Ohio',
+      city: 'Washington Court House',
+      bedrooms: 4,
+      bathrooms: 3.5,
+      year: 1868,
+      price: 75000,
+      description: 'Own a little piece of history -"The Cherry House"',
+      imgUrl: ''
+    }),
+    new House({
+      state: 'Iowa',
+      city: 'Vinton',
+      bedrooms: 4,
+      bathrooms: 1,
+      year: 1900,
+      price: 120000,
+      description: 'Historic Frank G. Ray House. 3-Story Queen Anne mansion and Carriage House.',
+      imgUrl: ''
+    })
+  ]
 }
 
 export const ProxyState = new Proxy(new AppState(), {
